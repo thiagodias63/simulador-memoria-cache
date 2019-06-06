89c94861a95084e651f39fce7faa3c1f14ec11bd
thiagodias63 <thidias6003@gmail.com>
2019-06-06 10:50:02 -0300

    Calculo de tamanho do bloco e tamanho da tag

--- src/components/MemoriaComponent.vue
+++ src/components/MemoriaComponent.vue
@@ -6,7 +6,7 @@
       <h4>Memória Principal</h4>
       <b-row>
         <b-col>
-          <b-form-group label="Tamanho da MP:" :description="memoria.tamanho + 'GB'">
+          <b-form-group label="Tamanho da MP:" :description="memoria.tamanho / (1024 * 1024) + 'GB'">
             <b-form-input v-model="memoria.tamanho" type="number"></b-form-input>
           </b-form-group>
         </b-col>
@@ -21,16 +21,13 @@
       <h4>Memória Cache</h4>
       <b-row>
         <b-col>
-          <b-form-group label="Tipo de Mapeamento:">
-            <b-form-select v-model="cache.tipo">
-              <option value="D" selected>Direta</option>
-              <option value="A">Associativa</option>
-            </b-form-select>
-          </b-form-group>
-        </b-col>
-        <b-col>
           <b-form-group label="Quantidade de Linhas:">
             <b-form-input v-model="cache.linha" type="number"></b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col v-if="cache.linha && memoria.quantidade">
+          <b-form-group label="N de Linhas:" :description="cache.quantidade / cache.n">
+            <b-form-input v-model="cache.n" type="number"></b-form-input>
           </b-form-group>
         </b-col>
         <b-col>
@@ -61,25 +58,31 @@ export default {
         quantidade: 0
       },
       cache: {
+        n: 0,
         linha: 0,
         tamanho: 0,
-        tag: 0,
-        tipo: 'D'
+        tag: 0
       }
     }
   },
   computed: {
     tamanhoBlocoMP: function () {
+      // 1048576 - 1GB
       if (this.memoria.tamanho > 0 && this.memoria.bloco > 0) {
-        let tamanhoMP = this.memoria.tamanho * 1024 * 1024 * 1024 // K M G
-        let tamanhoBloco = this.memoria.bloco
-
-        this.memoria.quantidade = tamanhoMP / tamanhoBloco
+        this.memoria.quantidade = this.memoria.tamanho / this.memoria.bloco
+        return this.memoria.quantidade
+      } else {
+        this.memoria.quantidade = 0
+        this.cache.tag = 0
+        return 0
+      }
+    },
+    tamanhoTag: function () {
+      if (this.memoria.quantidade > 0) {
         tamanhoMP = Math.log2(tamanhoMP)
         tamanhoBloco = Math.log2(tamanhoBloco)
-
         this.cache.tag = tamanhoMP - tamanhoBloco
-        return tamanhoMP - tamanhoBloco
+        return this.memoria.quantidade
       } else {
         this.memoria.quantidade = 0
         this.cache.tag = 0
